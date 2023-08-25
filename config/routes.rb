7139a851e871b10321678f55d5edc087074cf78e
Guterresthomas <guterresmikael@gmail.com>
2023-08-24 21:59:04 -0300

    sa

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :employees, only: [:index, :create, :show, :update, :destroy]
   resources :bulletin_boards, only: [:index, :create, :show, :update, :destroy]
-  resources :cash_register, only: [:index, :create, :show, :update, :destroy]
-  resources :customer, only: [:index, :create, :show, :update, :destroy]
-  resources :payment, only: [:index, :create, :show, :update, :destroy]
-  resources :product_category, only: [:index, :create, :show, :update, :destroy]
-  resources :product, only: [:index, :create, :show, :update, :destroy]
-  resources :sale_item, only: [:index, :create, :show, :update, :destroy]
-  resources :sale, only: [:index, :create, :show, :update, :destroy]
-  resources :sale_report, only: [:index, :create, :show, :update, :destroy]
-  resources :transaction_history, only: [:index, :create, :show, :update, :destroy]
+  resources :cash_registers, only: [:index, :create, :show, :update, :destroy]
+  resources :customers, only: [:index, :create, :show, :update, :destroy]
+  resources :payments, only: [:index, :create, :show, :update, :destroy]
+  resources :products_categories, only: [:index, :create, :show, :update, :destroy]
+  resources :products, only: [:index, :create, :show, :update, :destroy]
+  resources :sale_items, only: [:index, :create, :show, :update, :destroy]
+  resources :sales, only: [:index, :create, :show, :update, :destroy]
+  resources :sales_reports, only: [:index, :create, :show, :update, :destroy]
+  resources :transaction_histories, only: [:index, :create, :show, :update, :destroy]
   
   # Defines the root path route ("/")
   # root "articles#index"
