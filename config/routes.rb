9a16c919b6791fda07496c7aea8b467163a99b0a
Guterresthomas <guterresmikael@gmail.com>
2023-09-10 14:10:59 -0300

    koas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :users, only: [:new, :create, :show, :edit, :update, :destroy, :index]
   resources :employees, only: [:index, :create, :show, :update, :destroy]
   resources :bulletin_boards, only: [:index, :create, :show, :update, :destroy]
   resources :cash_registers, only: [:index, :create, :show, :update, :destroy]
