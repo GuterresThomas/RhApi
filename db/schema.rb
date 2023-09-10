8e4efbc94043b57efa5a6d99a4b52d8657bc1eea
Guterresthomas <guterresmikael@gmail.com>
2023-09-10 13:31:57 -0300

    ksoa

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_05_154358) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_10_163107) do
   create_table "bulletin_boards", force: :cascade do |t|
     t.string "title"
     t.string "body"
@@ -111,6 +111,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_154358) do
     t.string "event_type"
     t.text "description"
     t.datetime "timestamp"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
